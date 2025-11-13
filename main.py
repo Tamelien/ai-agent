3663e78659e88e7cb6cdf329cfaeaa671143b47e
Tamelien <Tamelien@protonmail.com>
2025-11-13 09:58:00 +0100

    feat(main.py): integrate model loading and prompt input via CLI

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
-def main():
-    print("Hello from ai-agent!")
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
 
+
+
+def main():
+
+    load_dotenv()
+
+    args = sys.argv[1:]
+    
+    if not args:
+        print("No prompt was entered.")
+        sys.exit(1)
+    
+    prompt = " ".join(args)
+
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001", 
+        contents= prompt
+    )  
+    print(response.text)
+    print(f"Prompt tokens: {getattr(response.usage_metadata, "prompt_token_count", "N/A")}")
+    print(f"Response tokens: {getattr(response.usage_metadata, "candidates_token_count","N/A")}")
 
 if __name__ == "__main__":
     main()
