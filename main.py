fe065a27524722aaca0462d5981deb9d28500a03
Tamelien <Tamelien@protonmail.com>
2025-11-13 12:19:45 +0100

    feat(main.py): add verbose flag to enable detailed logging

--- main.py
+++ main.py
@@ -1,32 +1,55 @@
 import os
 import sys
+import argparse
+from enum import Enum
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
+class FLAG(Enum):
+    VERBOSE = "--verbose"
 
+def parse_args(argv=None):
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("prompt", help="User prompt")
+    parser.add_argument(FLAG.VERBOSE.value, action="store_true", help="Verbose output")
+    return parser.parse_args(argv)
 
 def main():
 
     load_dotenv()
 
-    args = sys.argv[1:]
-    
-    if not args:
+    args = parse_args()
+    prompt = args.prompt
+   
+    if not prompt:
         print("No prompt was entered.")
         sys.exit(1)
     
-    prompt = " ".join(args)
-
+    messages = [
+    types.Content(role="user", parts=[types.Part(text=prompt)]),
+    ]
+        
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
     response = client.models.generate_content(
         model="gemini-2.0-flash-001", 
-        contents= prompt
-    )  
+        contents= messages
+    )
+       
+    if args.verbose:
+        um = getattr(response, "usage_metadata", None)
+        prompt_tokens = getattr(um, "prompt_token_count", "N/A") if um else "N/A"
+        response_tokens = getattr(um, "candidates_token_count", "N/A") if um else "N/A"
+
+        print(f"User prompt: {prompt}")
+        print(f"Prompt tokens: {prompt_tokens}")
+        print(f"Response tokens: {response_tokens}")
+
+    
     print(response.text)
-    print(f"Prompt tokens: {getattr(response.usage_metadata, "prompt_token_count", "N/A")}")
-    print(f"Response tokens: {getattr(response.usage_metadata, "candidates_token_count","N/A")}")
 
 if __name__ == "__main__":
     main()
