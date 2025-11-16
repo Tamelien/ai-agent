70ec5563c3a7376f874d538326dbd0cf2c8c2c17
Tamelien <Tamelien@protonmail.com>
2025-11-16 02:16:06 +0100

    feat(functions/) add get_file_content, write_file and run_python_file as tool schema

--- functions/available_functions.py
+++ functions/available_functions.py
@@ -1,7 +1,5 @@
 from google.genai import types
-from functions.get_files_info import get_files_info 
-
-
+from functions.config import MAX_CHARS
 
 schema_get_files_info = types.FunctionDeclaration(
     name="get_files_info",
@@ -17,8 +15,68 @@ schema_get_files_info = types.FunctionDeclaration(
     ),
 )
 
+schema_get_file_content = types.FunctionDeclaration(
+    name="get_file_content",
+    description=f"Read the file and return its contents limited to {MAX_CHARS} characters, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The file path to read the file from, relative to the working directory.",
+            ),
+        },
+        required=["file_path"],
+    ),
+)
+
+schema_write_file = types.FunctionDeclaration(
+    name="write_file",
+    description="Writes the content argument to a file at the specified file path, constrained to the working directory."\
+                "If the file path does not exist, it is created automatically. "\
+                "If the file already exists, its contents are overwritten with the value of the content argument.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The file path to write to, relative to the working directory."
+            ),
+
+            "content": types.Schema(
+                type=types.Type.STRING,
+                description="Content to write to the file",
+            ),
+        },
+        required=["file_path", "content"],
+    ),
+)
+
+schema_run_python_file = types.FunctionDeclaration(
+    name="run_python_file",
+    description="Executes a Python file at the specified path with optional list of command-line arguments, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The file path to the executed python file, relative to the working directory.", 
+            ),
+            "args": types.Schema(
+                type=types.Type.ARRAY,
+                items=types.Schema(type=types.Type.STRING),
+                description="Optional list of command-line arguments passed to the Python process."
+            )
+        },
+        required=["file_path"],
+    ),
+)
+
 available_functions = types.Tool(
     function_declarations=[
         schema_get_files_info,
+        schema_get_file_content,
+        schema_write_file,
+        schema_run_python_file
     ]
 )
